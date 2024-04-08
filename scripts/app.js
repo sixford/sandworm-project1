fb309d7338eba75ed6c6f93b9289734b56c5185c
Simon Oxford <simonoxford99@gmail.com>
2024-04-08 14:04:13 +0100

    Game functions, inc. game over. Styling + startBtn

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,10 @@
 // if game over, reveal game over screen
 // When restart button clicked, reset game
 
-const cells = []
+// Elements
+
+// const startButton = document.querySelector('#start')
+// const restartButton = document.querySelector('#restart')
 
 function init() {
 
@@ -17,75 +20,66 @@ function init() {
   const cellCount = width * width
   const cells = []
   for (let idx = 0; idx < cellCount; idx++) {
-    // Generate element
     const cell = document.createElement('div')
     cell.id = `cell-${idx}`
-    cell.dataset.index = idx
-    cell.classList.add('cell')
-
-    // Place the character in the starting cell
-    // if (idx === startingPos) {
-    //   cell.classList.add('water')
-    // }
-
-    // Set height and width of cell
-    // cell.style.width = `${100 / width}%`
-    // cell.style.height = `${100 / width}%`
-
-    // Add cell to the UI
+    cell.classList.add('grid')
     container.append(cell)
-
-    // Add the cell to the cells array
     cells.push(cell)
   }
 
 
-
   // Elements
-
-  // const startButton = document.querySelector('#start')
-  // const restartButton = document.querySelector('#restart')
 
 
   // Variables 
 
-  let sandwormBody = [305, 306]
+  let sandwormBody = [305, 306, 307]
   let water = [Math.floor(Math.random() * cells.length)]
+  let lastLoadTime = 0
   let score = 0
   let speed = 2
   let gameOver = false
   const scoreDisplay = document.querySelector('span')
 
-
-
   // add water + sandworm in cell in grid
   sandwormBody.forEach((idNumber) => {
     const cell = document.querySelector('#cell-' + idNumber)
     cell.classList.add('sandworm')
-    cell.style.backgroundColor = ('blue')
-    console.log(`Added sandworm to cell ${idNumber}`)
-    console.log(cell)
   })
-
   // addSandworm()
-
   water.forEach((idNumber) => {
     const cell = document.querySelector('#cell-' + idNumber)
     cell.classList.add('water')
-    console.log(`Added water to cell ${idNumber}`)
   })
 
-  // addWater()
+  const up = 38
+  const right = 39
+  const down = 40
+  const left = 37
 
-  // function generateWater() {
-  //   currentWater = Math.floor(Math.random() * cellCount)
-  //   cells[currentWater].classList.add('water')
-  // }
+  function reset() {
+    sandwormBody = [305, 306, 307]
+    water = [Math.floor(Math.random() * 399)]
+    lastLoadTime = 0
+    score = 0
+    speed = 2
+    gameOver = false
+    let gridsInnerHTML = ""
+    for (let i = 0; i < 400; i++) {
+      gridsInnerHTML += "<div id=" + "cell" + "-" + i + " class='grid'></div>"
+    }
+    document.querySelector('.container').innerHTML = gridsInnerHTML
+    sandwormBody.forEach((idNumber) => {
+      const cell = document.querySelector('#cell-' + idNumber)
+      cell.classList.add('sandworm')
+    })
+    water.forEach((idNumber) => {
+      const cell = document.querySelector('#cell-' + idNumber)
+      cell.classList.add('water')
+    })
+    document.querySelector('#gameOver').style.visibility = 'hidden'
+  }
 
-  // function generateSandworm() {
-  //   currentSandworm = startingPos
-  //   cells[currentSandworm].classList.add('sandworm')
-  // }
 
 
 
@@ -105,11 +99,79 @@ function init() {
   // }
   // console.log(startGame)
 
-  // function sandwormMovement(currentDirection) {}
+  function sandwormMovement(direction) {
+    console.log(direction)
 
-  //   const sandwormHead = sandwormBody[sandwormBody.length - 1]
-  //   const sandwormTail = sandwormBody[0]
-  //   let newSandwormHead
+    const sandwormHead = sandwormBody[sandwormBody.length - 1]
+    const sandwormTail = sandwormBody[0]
+    let newSandwormHead
+
+    if (direction === up) {
+      if (sandwormHead - width < 0) {
+        newSandwormHead = sandwormHead + cellCount - width
+      } else {
+        newSandwormHead = sandwormHead - width
+      }
+    } else if (direction === right) {
+      if (sandwormHead % width === width - 1) {
+        newSandwormHead = sandwormHead - width + 1
+      } else {
+        newSandwormHead = sandwormHead + 1
+      }
+    } else if (direction === down) {
+      if (sandwormHead + width >= cellCount) {
+        newSandwormHead = sandwormHead - cellCount + width
+      } else {
+        newSandwormHead = sandwormHead + width
+      }
+    } else if (direction === left) {
+      if (sandwormHead % width === 0) {
+        newSandwormHead = sandwormHead + width - 1
+      } else {
+        newSandwormHead = sandwormHead - 1
+      }
+    }
+
+
+    const drinkingWater = document.querySelector(`#cell-${newSandwormHead}`).classList.contains('water')
+    if (drinkingWater) {
+      document.querySelector(`#cell-${newSandwormHead}`).classList.remove('water')
+      sandwormBody.unshift(sandwormTail)
+      water.shift(newSandwormHead)
+      score++
+      scoreDisplay.innerText = score
+      speed++
+
+      let randomId = Math.floor(Math.random() * 399)
+      while (sandwormBody.includes(randomId)) {
+        randomId = Math.floor(Math.random() * 399)
+      }
+      water.unshift(randomId)
+      document.querySelector('#cell-' + randomId).classList.add('water')
+    } else {
+      sandwormBody.shift()
+      document.querySelector(`#cell-${sandwormTail}`).classList.remove('sandworm')
+    }
+    const eatingSelf = document.querySelector(`#cell-${newSandwormHead}`).classList.contains('sandworm')
+    if (eatingSelf) {
+      gameOver = true
+      document.querySelector('#gameOver').style.visibility = 'visible'
+      document.querySelector('#restart').addEventListener('click', () => { reset() })
+    }
+    sandwormBody.push(newSandwormHead)
+    document.querySelector(`#cell-${newSandwormHead}`).classList.add('sandworm')
+  }
+  //   if (currentDirection === right){
+  //     newSandwormHead = (sandwormHead + 1) % cellCount
+  //   } else if (currentDirection === left) {
+  //     newSandwormHead = (sandwormHead - 1 + cellCount) %
+  //   } else if (currentDirection === up) {
+  //     newSandwormHead = (sandwormHead - width + cellCount) %
+  //   } else if (currentDirection === down) {
+  //     newSandwormHead = (sandwormHead + width) % cellCount
+  //   }
+  // }
+
 
 
 
@@ -121,22 +183,33 @@ function init() {
   // - current position of sandworm - Made random cell
   // - intervalspeed
 
+  let currentDirection = up
+  function updateDirection(evt) {
+    if (evt.keyCode === up) {
+      currentDirection = up
+    } else if (evt.keyCode === right) {
+      currentDirection = right
+    } else if (evt.keyCode === down) {
+      currentDirection = down
+    } else if (evt.keyCode === left) {
+      currentDirection = left
+    }
+  }
 
-
-
-
-  // On Page Load
-  // window.onload = () => {
-  //   generateGrid()
-  // }
-
-
-
+  function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secsSinceLastLoading = (currentTime - lastLoadTime) / 1000
+    if (secsSinceLastLoading < 1 / speed) return
+    console.log('loading')
+    lastLoadTime = currentTime
+    if (!gameOver) {
+      sandwormMovement(currentDirection)
+    }
+  }
+  window.requestAnimationFrame(main)
 
 
   // Execution/Functions
-
-  // - game (main function) : called when start button is clicked. 
 
 
   // - Random position for water cell
@@ -155,13 +228,6 @@ function init() {
   //   newCell.classList.add['sandworm']
   // }
 
-
-  // - check for collision with water cell
-
-  // - check for collision with sandworm cell
-
-  // - if collision with water cell, add to sandworm cell 
-
   // - if sandworm cell added, increase speed/interval
 
   // - If collision with sandworm cell, game over
@@ -178,24 +244,13 @@ function init() {
   //   cells = []
   //   sandwormBody = [startingPos]
 
-  //   generateGrid()
-  // }
-
-
-  // - Function: if sandworm cell moves right off grid, reset on same row.
 
   // - Bring z-index (game over grid) to front
 
 
 
   // Events
-
-  // - Initialise game when start clicked
-  // startButton.addEventListener('click', startGame)
-  // - Handle user input; moving sandworm
-
-  // - When restart button clicked, reset grid
-  // restartButton.addEventListener('click', resetGame)
-
+  document.addEventListener('keydown', updateDirection)
 }
+
 window.addEventListener('DOMContentLoaded', init)
